3183d6a5bde319db049ff5469bf44a6158b2f004
Srijith.S <sreejith.pela@gmail.com>
2021-07-02 11:18:06 +0530

    Downgraded azure cli version in install-requirements.sh
    Latest Azure CLI python sdk is shoing azure login exception. So downgrading the cli sdk version

--- environment_setup/install-requirements.sh
+++ environment_setup/install-requirements.sh
@@ -1,5 +1,6 @@
 python --version
-pip install --upgrade azure-cli
+#pip install --upgrade azure-cli
+pip install azure-cli==2.20.0
 pip install --upgrade azureml-sdk[cli]
 pip install pytest
 pip install pytest-cov
